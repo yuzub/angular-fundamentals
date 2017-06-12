b3daf154d7cf707fa0c26fd8e291b8df952a3fd3
Yuriy Zubaha <yuriy.zubaha@gmail.com>
2017-06-13 02:06:36 +0300

    Change dev-server

--- scripts/web-server.js
+++ scripts/web-server.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var path = require('path');
+var open = require('open');
 // var events = require('./eventsController');
+var port = 8000;
 var app = express();
 var rootPath = path.normalize(__dirname + '/../');
 // var bodyParser = require('body-parser');
@@ -14,5 +16,40 @@ app.get('/data/event', events.getAll);
 app.post('/data/event/:id', events.save);
 app.get('*', function(req, res) { res.sendFile(rootPath + '/app/index.html'); });
 */
-app.listen(8000);
-console.log('Listening on port ' + 8000 + '...');
+app.listen(port, function(err) {
+	if (err) {
+		console.log(err);
+	} else {
+		console.log('Listening on port ' + 8000 + '...');
+		open('http://localhost:' + port);
+	}
+});
+
+
+// server from course Building js-dev-environment
+/*import express from 'express';
+import path from 'path';
+import open from 'open';
+import webpack from 'webpack';
+import config from '../webpack.config.dev';
+
+const port = 3000;
+const app = express();
+const compiler = webpack(config);
+
+app.use(require('webpack-dev-middleware')(compiler, {
+	noInfo: true,
+	publicPath: config.output.publicPath
+}));
+
+app.get('/', function(req, res) {
+	res.sendFile(path.join(__dirname, '../src/index.html'));
+});
+
+app.listen(port, function(err) {
+	if (err) {
+		console.log(err);
+	} else {
+		open('http://localhost:' + port);
+	}
+});*/
